9ff0c4cbc642d687193612baf4850d39163a5f0a
bilAhmet <49236536+bilAhmet@users.noreply.github.com>
2019-11-22 18:10:04 +0300

    Update multi_face_detection_python.py
    detecting faces in an image and cropping them then saving them as different images in the same working directory

--- multi_face_detection_python.py
+++ multi_face_detection_python.py
@@ -2,18 +2,19 @@
 import numpy as np
 import cv2 
 import matplotlib.pyplot as plt
-get_ipython().magic('matplotlib inline')
+import time
+
 
 
 
 #  Loading the image to be tested
-test_image = cv2.imread('baby1.jpg')
+#test_image = cv2.imread('pic.png')
 
 # Converting to grayscale as opencv expects detector takes in input gray scale images
-test_image_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+#test_image_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
 
 # Displaying grayscale image
-plt.imshow(test_image_gray, cmap='gray')
+#plt.imshow(test_image_gray, cmap='gray')
 
 
 # Since we know that OpenCV loads an image in BGR format so we need to convert it into RBG format to be able to display its true colours. Let us write a small function for that.
@@ -28,34 +29,28 @@ def convertToRGB(image):
 
 # Loading the classifier for frontal face
 
-
-haar_cascade_face = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_alt2.xml')
+#assuming it is in the same directory
+haar_cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
 
 # # Face detection
 
 
-faces_rects = haar_cascade_face.detectMultiScale(test_image_gray, scaleFactor = 1.2, minNeighbors = 5);
+#faces_rects = haar_cascade_face.detectMultiScale(test_image_gray, scaleFactor = 1.2, minNeighbors = 5);
 
 # Let us print the no. of faces found
-print('Faces found: ', len(faces_rects))
-
-
+#print('Faces found: ', len(faces_rects))
 
 # Our next step is to loop over all the co-ordinates it returned and draw rectangles around them using Open CV.We will be drawing a green rectangle with thicknessof 2
 
-
-
-for (x,y,w,h) in faces_rects:
-     cv2.rectangle(test_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#for (x,y,w,h) in faces_rects:
+#     cv2.rectangle(test_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
         
-
-
 # Finally, we shall display the original image in coloured to see if the face has been detected correctly or not.
 
 
 #convert image to RGB and show image
-plt.imshow(convertToRGB(test_image))
+#plt.imshow(convertToRGB(test_image))
 
 
 #  Let us create a generalised function for the entire face detection process.
@@ -70,18 +65,23 @@ def detect_faces(cascade, test_image, scaleFactor = 1.1):
     
     # Applying the haar classifier to detect faces
     faces_rect = cascade.detectMultiScale(gray_image, scaleFactor=scaleFactor, minNeighbors = 5)
-    
+    # saving every face cropped from the main frame in the same directory
+    for (x, y, w, h) in faces_rect:
+        sub=image_copy[y:y+h, x:x+w]
+        cv2.imwrite(str(time.time())+'.png',sub)
+        
     for (x, y, w, h) in faces_rect:
         cv2.rectangle(image_copy, (x, y), (x+w, y+h), (0, 255, 0), 15)
         
     return image_copy
 
 
+
 # Testing the function on new image
 
 
 #loading image
-test_image2 = cv2.imread('group.jpg')
+test_image2 = cv2.imread('old.png')
 
 #call the function to detect faces
 faces = detect_faces(haar_cascade_face, test_image2)
@@ -91,4 +91,5 @@ plt.imshow(convertToRGB(faces))
 
 # Saving the final image
 
-cv2.imwrite('image1.png',faces)
+cv2.imwrite('newPic.png',faces)
+
